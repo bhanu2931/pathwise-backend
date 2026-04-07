50b328682803e1c7bfe7da03c6915315c84b29ef
bhanu2931 <bhanuprakashgajula00@gmail.com>
2026-04-07 19:31:00 +0530

    fixed user import

--- src/main/java/com/pathwise/backend/controller/AuthController.java
+++ src/main/java/com/pathwise/backend/controller/AuthController.java
@@ -1,51 +1,30 @@
 package com.pathwise.backend.controller;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
-
 import com.pathwise.backend.model.User;
-import com.pathwise.backend.repository.UserRepository;
-
-import java.util.List;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/auth")
 @CrossOrigin(origins = "*")
 public class AuthController {
 
-    @Autowired
-    private UserRepository repo;
+    private String savedEmail = "admin@gmail.com";
+    private String savedPassword = "1234";
 
-    // REGISTER
     @PostMapping("/register")
     public String register(@RequestBody User user) {
-
-        User existing = repo.findByEmail(user.getEmail());
-
-        if (existing != null) {
-            return "User already exists";
-        }
-
-        repo.save(user);
+        savedEmail = user.getEmail();
+        savedPassword = user.getPassword();
         return "Registered Successfully";
     }
 
-    // LOGIN
     @PostMapping("/login")
     public String login(@RequestBody User user) {
 
-        User existing = repo.findByEmail(user.getEmail());
-
-        if (existing != null && existing.getPassword().equals(user.getPassword())) {
+        if (user.getEmail().equals(savedEmail) &&
+            user.getPassword().equals(savedPassword)) {
             return "Login Success";
         } else {
             return "Invalid Credentials";
         }
-    }
-
-    // ADMIN: GET ALL USERS
-    @GetMapping("/users")
-    public List<User> getUsers() {
-        return repo.findAll();
     }
 }
--- src/main/java/com/pathwise/backend/repository/UserRepository.java
+++ src/main/java/com/pathwise/backend/repository/UserRepository.java
@@ -1,8 +1,10 @@
 package com.pathwise.backend.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import com.pathwise.backend.model.User;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
     User findByEmail(String email);
